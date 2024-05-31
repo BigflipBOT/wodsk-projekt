f0d1cfa14b602fba89dd0be60206a98980ef00e9
BigflipBOT <filip.pyra@gmail.com>
2024-06-01 00:45:35 +0200

    added both sjf alghoritms

--- zad1/src/main.rs
+++ zad1/src/main.rs
@@ -1,10 +1,10 @@
 #![allow(dead_code)]
 pub mod proc;
-
-use std::u64;
-
 use crate::proc::Process;
 use crate::proc::ProcessState;
+
+use std::cell::RefCell;
+use std::usize;
     
 fn all_is_finished<'a>(processes: &'a Vec<Process>) -> bool {
     processes.iter().all(|process| process.state() == ProcessState::Finished)
@@ -16,46 +16,62 @@ fn all_is_finished<'a>(processes: &'a Vec<Process>) -> bool {
     // return true;
 }
 
-fn check_arrival(processes: &mut Vec<Process>, cur_time: u64) {
+fn check_arrival(processes: &mut Vec<Process>, cur_time: u64) -> usize {
+    let mut counter: usize = 0;
     for process in processes.iter_mut() {
         if process.state() == ProcessState::Unborn {
             if process.arrival() <= cur_time {
                 (*process).deactivate();
+                counter += 1;
             }
         }
     }
+    counter
+}
+
+fn avg_wait_time(processes: &Vec<Process>) -> f64 {
+    let mut sum: f64 = 0.0;
+    for process in processes.iter() {
+        sum += process.standby_time() as f64;
+    }
+    sum / (processes.len() as f64)
 }
 
 fn import_data (data: &mut Vec<Process>) {
-    // data = &Vec<process>::new();
-    data.push(Process::new(0, 3));
-    data.push(Process::new(1, 2));
-    data.push(Process::new(4, 7));
-    data.push(Process::new(6, 3));
+    // let data = &Vec<Process>::new();
+    // data.push(Process::new(0, 3));
+    // data.push(Process::new(1, 2));
+    // data.push(Process::new(4, 7));
+    // data.push(Process::new(6, 3));
+
+    data.push(Process::new(0, 8));
+    data.push(Process::new(3, 5));
+    data.push(Process::new(5, 1));
+    data.push(Process::new(12, 3));
+    data.push(Process::new(19, 1));
 }
 
 fn round_robin(mut dataset: Vec<Process>) -> u64 {
     let mut time: u64 = 0;
 
+    let mut new_process_counter: usize = 0;
     let kwant: u64 = 2;
     let mut chamber_time: u64 = 0;
     let mut counter: usize = 0;
 
+    dataset.push(Process::new(0, 8));
     while !all_is_finished(&dataset) {
         check_arrival(&mut dataset, time);
         
         //alghoritm
-        if dataset.iter().all(|process| process.state() != ProcessState::Active) {
-            for process in dataset.iter_mut() {
-                if process.state() == ProcessState::Inactive {
-                    println!("no active process, activating first process");
-                    (*process).activate();
-                    break;
-                }
-            }
+        let mut process_que: Vec<RefCell<Process>> = Vec::new(); 
+        for process in dataset.iter_mut() {
+            if process.state() == ProcessState::Inactive { process_que.push(RefCell::new(*process)) }
         }
-        if chamber_time >= kwant {
-            unimplemented!()
+
+        while process_que.iter().all(|process| (process.borrow()).state() != ProcessState::Finished) {
+            let mut iterations = 0;
+            process_que[iterations].borrow_mut().increment();
         }
         //
         time += 1;
@@ -64,8 +80,9 @@ fn round_robin(mut dataset: Vec<Process>) -> u64 {
     unimplemented!();
 }
 
-fn fcfs(mut dataset: Vec<Process>) -> u64 {
+fn fcfs(mut dataset: Vec<Process>) -> f64 { // TODO: what happens when there are two processes arriving ath the same time?
     let mut time: u64 = 0;
+    dataset.sort_by_key(|process| process.arrival());
 
     while !all_is_finished(&dataset) {
         check_arrival(&mut dataset, time);
@@ -74,7 +91,7 @@ fn fcfs(mut dataset: Vec<Process>) -> u64 {
         if dataset.iter().all(|process| process.state() != ProcessState::Active) {
             for process in dataset.iter_mut() {
                 if process.state() == ProcessState::Inactive {
-                    println!("no active process, activating first process");
+                    // println!("no active process, activating first process");
                     (*process).activate();
                     break;
                 }
@@ -82,10 +99,8 @@ fn fcfs(mut dataset: Vec<Process>) -> u64 {
         }
 
         for process in dataset.iter_mut() {
-            if process.state() == ProcessState::Active {
-                println!("incrementing process");
-                (*process).increment();
-            }
+            // println!("incrementing process");
+            (*process).increment();
         }
 
         //debug things and stuff below 
@@ -95,20 +110,70 @@ fn fcfs(mut dataset: Vec<Process>) -> u64 {
         // }
         time += 1;
     }
-    time / (dataset.len() as u64)
+    avg_wait_time(&dataset)
 }
 
-fn sjf_nw() {
-    unimplemented!();
+fn sjf_nw(mut dataset: Vec<Process>) -> f64 {
+    let mut time: u64 = 0;
+
+    while !all_is_finished(&dataset) {
+        check_arrival(&mut dataset, time);
+        
+        //alghoritm
+        if dataset.iter().all(|process| process.state() != ProcessState::Active) {
+            if let Some(process) = dataset.iter_mut().filter(|process| process.state() == ProcessState::Inactive).min_by_key(|process| process.length()) {
+                process.activate();
+            }
+        }    
+
+        for process in dataset.iter_mut() {
+            (*process).increment();
+        }
+        time += 1;
+    }
+    avg_wait_time(&dataset)    
 }
 
-fn sjf_w() {
-    unimplemented!();
+fn sjf_w(mut dataset: Vec<Process>) -> f64 { // TOdo ask if alghoritm is corect (about weightsystem)
+   let mut time: u64 = 0;
+
+    while !all_is_finished(&dataset) {
+        check_arrival(&mut dataset, time);
+        
+        //alghoritm
+        // if let Some(shortest_process) = dataset.iter_mut().filter(|process| process.state() == ProcessState::Inactive).min_by_key(|process| process.length()) {
+        //     // change the state of the shortest inactive process to activa                                           this code below (.min_by_key(...)) is kind of hacky solltution to keep compiler happy
+        //     if let Some(active_process) = dataset.iter_mut().filter(|process| process.state() == ProcessState::Active).min_by_key(|process| process.length()) {
+        //         // change the state of the shortest inactive process to active
+        //         active_process.activate();
+        //     }
+        //     shortest_process.activate();
+        // }
+        if let Some(active_process) = dataset.iter_mut().filter(|process| process.state() == ProcessState::Active).min_by_key(|proc| proc.length()) {
+            active_process.deactivate();
+        }
+        if dataset.iter().all(|process| process.state() != ProcessState::Active) { // the same alghoritm as for sjf_nw
+            if let Some(process) = dataset.iter_mut().filter(|process| process.state() == ProcessState::Inactive).min_by_key(|process| process.length()) {
+                process.activate();
+            }
+        }    
+        //if the currently active alghoritm was shortest nothing changes after above 8 lines
+
+
+        for process in dataset.iter_mut() {
+            (*process).increment();
+        }
+        time += 1;
+    }
+    avg_wait_time(&dataset)
 }
+
 
 fn main() {
     let mut data: Vec<Process> = Vec::new();
     import_data(&mut data);
-    // println!("{:?}", data);
-    println!("avg response time for fcfs: {}", fcfs(data));
+    println!("avg response time for fcfs: {}", fcfs(data.clone()));
+    println!("avg response time for sjf_nw: {}", sjf_nw(data.clone()));
+    println!("avg response time for sjf_w: {}", sjf_w(data.clone()));
+
 }
--- zad1/src/proc.rs
+++ zad1/src/proc.rs
@@ -1,18 +1,23 @@
 #[derive(Copy, Clone)]
 pub struct Process {
     state: ProcessState,        //state of the process
-    arrival_time: u64,          //when process arrives in ms(?)
-    length: u64,                //how much time is needet for completion in ms(?)
-    completion: u64,            //how complete is a process in ms(?)
+    arrival_time: u64,          //when process arrives 
+    length: u64,                //how much time is needet for completion 
+    completion: u64,            //how complete is a process 
+    standby_time: u64,          //how much process is waiting existing and not being completed
 }
 impl Process {
     pub fn new(arrival: u64, len: u64) -> Self {
-        Process { state: ProcessState::Unborn, arrival_time: arrival, length: len, completion: 0 }
+        Process { state: ProcessState::Unborn, arrival_time: arrival, length: len, completion: 0, standby_time: 0, }
     }
     pub fn increment(&mut self) {
-        self.completion += 1;
-        if self.completion >= self.length {
-            self.state = ProcessState::Finished;
+        if self.state == ProcessState::Active { // incresing completion on process when the process is in active state
+            self.completion += 1;
+            if self.completion >= self.length {
+                self.state = ProcessState::Finished;
+            }
+        } else if self.state == ProcessState::Inactive { // and incresing the standby time when process spawned but is not active
+            self.standby_time += 1;
         }
     }
     pub fn state(&self) -> ProcessState {
@@ -27,12 +32,13 @@ impl Process {
     pub fn arrival(&self) -> u64 {
         self.arrival_time
     }
+    pub fn standby_time(&self) -> u64 { self.standby_time }
+    pub fn length(&self) -> u64 { self.length }
 }
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
 pub enum ProcessState {
-    Active,
-    Inactive,
-    Finished,
-    Unborn,
+    Active,     // process is being completed
+    Inactive,   // process spawned but is not being completed
+    Finished,   // process is completed (finished)
+    Unborn,     // process is not yet spawned 
 }
-
